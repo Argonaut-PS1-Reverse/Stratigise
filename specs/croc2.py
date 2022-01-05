732f59fd411f70eff70d06589bf746e573055ead
knot126 <73200930+knot126@users.noreply.github.com>
2022-01-05 00:03:53 +0000

    Update croc2 strat spec

--- specs/croc2.py
+++ specs/croc2.py
@@ -1,9 +1,17 @@
-{
-	'InstructionSize': 4,
-	'EvalType': 'unknown',
+"""
+Croc 2 opcodes
+"""
+
+instructionSize = 4
+
+opcodes = {
 	0x00000000: ['CommandError'], # This should never be decompiled in an offical wad.
 	0x00000001: ['Local', 'int32'], # Push local variable onto the stack
 	0x00000002: ['Global', 'int32'], # Push global variable onto the stack
 	0x00000003: ['WorldGlobal', 'int32'], # Push world global variable onto the stack
 	0x00000004: ['AlienVar', 'int32'], # Push alien variable onto the stack
 }
+
+# Not needed for Croc 2 ?
+def unevaluate(strat):
+	pass
